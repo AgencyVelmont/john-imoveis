84437d6223ce9452c1b3263125b53165619ee97e
Messias Rocha <messias25upgmail.com@macbook-air-messias.tailee448e.ts.net>
2026-06-09 02:21:46 -0300

    Corrige build do admin no deploy da Hostinger

--- admin/public/sw.js
+++ admin/public/sw.js
@@ -1,4 +1,4 @@
-const APP_SHELL_CACHE = "felipe-admin-shell-v2";
+const APP_SHELL_CACHE = "felipe-admin-shell-v3";
 const ADMIN_ORIGIN = "https://admin.felipecorretor.com.br";
 const ADMIN_LEADS_URL = `${ADMIN_ORIGIN}/leads`;
 const SPA_FALLBACK_URL = "/index.html";
